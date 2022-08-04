bd020c22f3b9c59fef350241fc5f5cf2ca7c92a3
netbrain <kim@heldig.org>
2022-08-04 08:54:02 +0200

    initial import

---
+++ README.md
@@ -0,0 +1,19 @@
+== WSL2Workaround for Cisco Anyconnect
+
+The following steps will enable your system to run a script everytime the cisco anyclient is connected to the vpn.
+The script will alter the priority of the vpn interface to enable wsl2 to use it, and it will configure the wsl instance
+to use the dns servers configured in the vpn interface.
+
+1. Copy the wsl2workaround.ps1 script to C:\dev
+2. Open scheduled tasks and import wsl2workaround.xml
+3. Disable creation of resolv.conf in your WSL2 instance by creating the `/etc/wsl.conf` with the following contents
+
+[network]
+generateResolvConf = false
+
+4. Remove (if present) the /etc/resolv.conf file
+5. Create a new symlink with `ln -s /mnt/c/dev/wsl2workaround-resolv.conf /etc/resolv.conf`
+
+=== Gotcha's
+
+1. You need to launch your wsl instance prior to connecting to VPN for this to work. 
---
+++ wsl2workaround.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+    Cisco Anyconnect Workaround
+.DESCRIPTION
+    Start or stop the Cisco Anyconnect Workaround
+.PARAMETER Action
+    "start" or "stop"
+.PARAMETER File
+    Optional, path to resolv file
+
+#>
+
+Param(
+    [String]$Action = "",
+    [String]$File = "C:\dev\wsl2workaround-resolv.conf"
+)
+
+if ( "start" -ilike $Action )
+{
+    Get-NetAdapter | ? {$_.InterfaceDescription -like "*Cisco*" } |Set-NetIPInterface -InterfaceMetric 6000
+
+    $dns = (Get-NetIPConfiguration | Where-Object {$_.InterfaceDescription -like "Cisco*"} | Select @{l="IP";e={$_.DNSServer.Serveraddresses}})
+    Clear-Content -Path $File
+    foreach($u in $dns.IP){
+        Add-Content -Path $File -Value "nameserver $u`n" -Encoding "ascii" -NoNewline
+    }
+}
+elseif ( "stop" -ilike $Action )
+{
+    Get-NetAdapter | ? {$_.InterfaceDescription -like "*Cisco*" } |Set-NetIPInterface -InterfaceMetric 1
+    Clear-Content -Path $File
+    Set-Content -Path $File -Value "nameserver 8.8.8.8`n" -Encoding "ascii" -NoNewline
+}
+else
+{
+    Get-help $PSCommandPath -Detailed
+}
+
